74f369f56d4c8b520a3cf2198e98ca421bebcf17
EdAraujo <ed.araujo369@gmail.com>
2023-10-06 10:14:56 -0300

    Commit 3

--- src/App.java
+++ src/App.java
@@ -1,16 +1,44 @@
+import java.util.concurrent.ThreadLocalRandom;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("PROCESSO SELETIVO \n");
-
-        analisarCandidato(1900.0);
-        analisarCandidato(2000.0);
-        analisarCandidato(2200.0);
+        selecaoCandidatos();
+        
     }
+
+    static void selecaoCandidatos(){
+        String [] candidatos = {"Edson", "Antonio", "Gemerson", "Lucas", "Gutemberg", "Waldeir", "Carioca", "Manuel", "Wolf", "André"};
+        
+        int candidatosSelecionados = 0;
+        int candidatosAtual=0;
+        double salario = 2000.0;
+
+        while(candidatosSelecionados < 5 && candidatosAtual < candidatos.length){
+            String candidato = candidatos[candidatosAtual];
+            double salarioPretendido = valorPretendido();
+            System.out.println("O candidato: " + candidato + " solicitou o valor: "+ salarioPretendido);
+
+            if (salario >= salarioPretendido){
+                System.out.println("O candidato: "+ candidato + " foi selecionado!");
+                candidatosSelecionados++;
+            }
+            
+                
+                candidatosAtual++;
+            
+        }
+    }
+
+    static double valorPretendido(){
+        return ThreadLocalRandom.current().nextDouble(1800, 2200);
+    }
+
 
     static void analisarCandidato(double salarioPretendido){
         double salario;
 
-        salario = 2000;
+        salario = 1000;
 
         if (salario > salarioPretendido){
             System.out.println("LIGAR PARA O CANDIDATO \n");
