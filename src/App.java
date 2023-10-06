cabcd274a172ffddc398441e953dffd2ef6d3889
EdAraujo <ed.araujo369@gmail.com>
2023-10-06 09:45:09 -0300

    Commit 2

--- src/App.java
+++ src/App.java
@@ -1,6 +1,27 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("main");
-        
+        System.out.println("PROCESSO SELETIVO \n");
+
+        analisarCandidato(1900.0);
+        analisarCandidato(2000.0);
+        analisarCandidato(2200.0);
+    }
+
+    static void analisarCandidato(double salarioPretendido){
+        double salario;
+
+        salario = 2000;
+
+        if (salario > salarioPretendido){
+            System.out.println("LIGAR PARA O CANDIDATO \n");
+        }
+
+        else if(salario == salarioPretendido){
+            System.out.println("LIGAR PARA O CANDIDATO, COM CONTRA PROPOSTA \n");
+        }
+
+        else {
+            System.out.println("AGUARDANDO RESULTADO DOS DEMAIS CANDIDATOS \n");
+        }
     }
 }
